56e196895279bbe3a85445d4dd642d19acdd0d84
Prasadkare77 <prasadkare77@gmail.com>
2023-11-14 21:41:34 +0530

    Create own react library and JSX

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,9 @@
 import NewTest from './NewTest'
 function App() {
-  
-
+  const username = 'prasad'
   return (
     <>
-      <h1>React Course With Vite</h1>
+      <h1>React Course With Vite {username}</h1>
       <NewTest/>
     </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { jsx as _jsx  } from 'react/jsx-runtime.js'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+function MyApp() {
+  return(
+    <>
+      <div>
+        <h1>Custom App</h1>
+      </div>
+    </>
+  )
+}
+
+// const reactElement = {
+//   type: 'div',
+//   props: {
+//       href: 'https://google.com',
+//       target: '_blank'
+//   },
+//   childern: 'Click me to visit google'
+// }
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+const reactElement = React.createElement(
+  'a', 
+  {href:'https:/google.com', target: '_blank'},
+  'Click me to Visit'
+)
+
+const anotherUser = 'prasad'
+
+ReactDOM.createRoot(document.getElementById('root'))
+.render(
+  <App/>
 )
