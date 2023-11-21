3df9bbae22d5aad2c5638f4c7b72971fcbe5e0fe
Prasadkare77 <prasadkare77@gmail.com>
2023-11-21 23:01:27 +0530

    useEffect, useRef and useCallback

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <h3 className="text-center justify-center">Background Color Changer</h3>
-      <div className=" h-screen duration-1000" style={{backgroundColor:color}}>
+      <div className="w-full h-screen duration-1000" style={{backgroundColor:color}}>
         <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
           <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-lg">
             <button onClick={()=>setColor('red')} className="outline-none px-4 py-1 rounded-full text-white shadow-sm" style={{backgroundColor:'red'}}>Red</button>
