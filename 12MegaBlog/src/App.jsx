67c29e301629ca50697a7df1760499cba6dcb54e
Prasadkare77 <prasadkare77@gmail.com>
2023-12-06 21:27:45 +0530

    blogs with appwrite

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_REACT_APP_APPWRITE_URL);
+
+  return (
+    <>
+      <h1>A Blog App with Appwrite</h1>
+    </>
+  )
+}
+
+export default App
