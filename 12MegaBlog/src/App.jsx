62a8f2cdc1c262bd47020925cb0ced6ff0d59cac
Prasadkare77 <prasadkare77@gmail.com>
2023-12-11 18:11:18 +0530

    created Redux Store for user Sessions (login, out)

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,11 +1,41 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux';
+import authService, { AuthService } from './appwrite/auth';
+import { login, logout } from './store/authSlice';
+import Header from './components/Header/Header.jsx'
+import Footer from './components/Footer/Footer.jsx'
+import { Provider } from 'react-redux';
+import store from './store/store.js'
 
 function App() {
-  console.log(import.meta.env.VITE_REACT_APP_APPWRITE_URL);
+  const dispatch = useDispatch()
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+
+
 
   return (
     <>
-      <h1>A Blog App with Appwrite</h1>
+      return loading ? (
+        <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+          <div className='w-full block'>
+            <Header/>
+            <Footer/>
+          </div>
+        </div>
+      ) : null;
     </>
   )
 }
