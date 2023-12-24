36b8f28338f3c32ba91cd3dd2e3e47d485f4538d
Prasadkare77 <prasadkare77@gmail.com>
2023-12-24 22:21:36 +0530

    Login Component authentication

---
+++ 12MegaBlog/src/components/AuthLayout.jsx
@@ -0,0 +1,32 @@
+import React, {useEffect,useState} from 'react'
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+export default Protected = ({childern, authentication = true}) => {
+    const navigate = useNavigate()
+    const [loder,setLoader] = useState(true)
+    const authStatus = useSelector(state => state.auth.status)
+
+    useEffect(() => {
+        // TODO: make it more east to understand
+        // if (authStatus === true) {
+        //     navigate('/')
+        // } else if (authStatus === false){
+        //     navigate('/login')
+        // }
+
+        if(authentication && authStatus !== authentication){
+            navigate('/login')
+        } else if(!authentication && authStatus !== authentication){
+            navigate('/')
+        }
+        setLoader(false)
+    }, [authStatus, navigate, authentication])
+
+  return (
+    <div>
+        loader ? <h1>Loading</h1> : <>{childern}</>
+    </div>
+  )
+}
+
