3d02db7f126b4c58b9366cbd61cc7882154fc349
Prasadkare77 <prasadkare77@gmail.com>
2024-01-22 21:11:39 +0530

    cors

--- 12MegaBlog/src/components/RTE.jsx
+++ 12MegaBlog/src/components/RTE.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
 import { Editor } from '@tinymce/tinymce-react'
-import {Controller } from 'react-hook-form';
+import { Controller } from 'react-hook-form';
 
 const RTE = ({name, control, label, defualtValue = ''}) => {
   return (
     <div className='w-full'>
         <Editor
-        initialValue='default value'
-        init={
-            {branding: false,
-            height: 500,
-            menubar: true,
-            plugins: [],
-            toolbar: ''
-        }
-        }>
-
+          initialValue='default value'
+          init={
+              {branding: false,
+              height: 500,
+              menubar: true,
+              plugins: [],
+              toolbar: ''
+          }
+          }>
         </Editor>
 
         {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
